7baec45247bd564e04d897381db00c5ea6211358
TheHypnotic <72342378+TheHypnotic@users.noreply.github.com>
2025-09-15 14:46:52 +0330

    s

--- training-api/image_classification.py
+++ training-api/image_classification.py
@@ -58,6 +58,7 @@ def get_dataset_download_urls(
     response = requests.post(url, json=payload, headers=headers, timeout=10, proxies={"http": None, "https": None})
     # response.raise_for_status()
     data = response.json()
+    print(data)
     
     # Filter for .tar.gz and .csv files only
     download_urls = [
