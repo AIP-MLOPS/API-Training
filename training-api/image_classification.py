cd4748e3d3919ebdfc877ae9e7f212177e3eb2bc
TheHypnotic <72342378+TheHypnotic@users.noreply.github.com>
2025-09-15 14:14:24 +0330

    w

--- training-api/image_classification.py
+++ training-api/image_classification.py
@@ -195,7 +195,7 @@ cfg = {
 }
 # Data URL
 url = get_dataset_download_urls(
-    url="http://172.15.30.79:8169/download-dataset",
+    url="api.mlops.ai-lab.ir/data/download-dataset",
     dataset_name=cfg["dataset_config"]["name"],
     user_name=data_model_reg_cfg['clearml_username'],
     clearml_access_key=data_model_reg_cfg['clearml_access_key'],
