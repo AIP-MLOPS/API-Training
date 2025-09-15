2672d8a4a6300b08c03f0f28c6dcadf1882ee146
TheHypnotic <72342378+TheHypnotic@users.noreply.github.com>
2025-09-15 16:43:29 +0330

    a

--- training-api/timeseries.py
+++ training-api/timeseries.py
@@ -1,24 +1,132 @@
-
+import os
 import torch
 import torch.nn as nn
 import torchvision.models as models
 from torch.nn import functional as F
 from dotenv import load_dotenv
+import requests
+import json
+
+from clearml import Task
 
 from ml_trainer import AutoTrainer
 from ml_trainer.base import AbstractModelArchitecture
 from aipmodel.model_registry import MLOpsManager
-from clearml import Task
+
+#  ====================== Data Registry =========================
+
+def get_dataset_download_urls(
+    url: str,
+    dataset_name: str,
+    user_name: str,
+    clearml_access_key: str,
+    clearml_secret_key: str,
+    s3_access_key: str,
+    s3_secret_key: str,
+    s3_endpoint_url: str,
+    download_method: str = "presigned_urls"
+):
+    """
+    Request presigned download URLs for a dataset.
+
+    Returns:
+        List of download URLs for .tar.gz and .csv files only
+    """
+    base = "http://data-ingestion-api-service.aip-mlops-service.svc.cluster.local:8169/download-dataset"
+
+    # 1) Confirm what you’re actually hitting
+    r = requests.post(f"{base}/download-dataset", json={}, timeout=10,
+                  proxies={"http": None, "https": None})
+    payload = {
+        "dataset_name": dataset_name,
+        "user_name": user_name,
+        "clearml_access_key": clearml_access_key,
+        "clearml_secret_key": clearml_secret_key,
+        "s3_access_key": s3_access_key,
+        "s3_secret_key": s3_secret_key,
+        "s3_endpoint_url": s3_endpoint_url,
+        "download_method": download_method
+    }
+    print(payload)
+    
+    headers = {
+        "accept": "application/json",
+        "Content-Type": "application/json"
+    }
+    
+    response = requests.post(url, json=payload, headers=headers, timeout=10, proxies={"http": None, "https": None})
+    # response.raise_for_status()
+    data = response.json()
+    print(data)
+    
+    # Filter for .tar.gz and .csv files only
+    download_urls = [
+        file["download_url"]
+        for file in data.get("download_info", {}).get("files", [])
+        if file["filename"].endswith(".tar.gz") or file["filename"].endswith(".csv")
+    ]
+    return download_urls[0]
+
+
+
+# --------- ClearML task initialization --------
+task = Task.init(
+    project_name="API training",  # Name of the ClearML project
+    task_name=f"API Training",  # Name of the task
+    task_type=Task.TaskTypes.optimizer,  # Type of the task (could also be "training", "testing", etc.)
+    reuse_last_task_id=False  # Whether to reuse the last task ID (set to False for a new task each time)
+)
 
 load_dotenv()
 
+data_model_reg_cfg= {
+    #ceph related
+    'CEPH_ENDPOINT': 'http://172.15.20.153',
+    'CEPH_ACCESS_KEY': '8HZE0U3P5VOSCPEOUN4G',
+    'CEPH_SECRET_KEY': 'ihLqlXsauVYmiV83uu5kDdzAZzjlLlXYx05OIOwg',
+    'CEPH_BUCKET': 'bucket',
+
+    #clearml
+    'clearml_access': '6C8CD3D76920C6D1F81FBBBCD188734C',
+    'clearml_secret': 'F3F98D9B5361768BF497DFFB8F63F6E9115C5314FDED76FF18E3B82997DFB6D5',
+    'clearml_username': 'testdario7',
+    'clearml_url': 'http://web.mlops.ai-lab.ir/api_old'
+}
+data_model_reg_cfg= {
+    #ceph related
+    'CEPH_ENDPOINT': 'url',
+    'CEPH_ACCESS_KEY': 'access',
+    'CEPH_SECRET_KEY': 'secret',
+    'CEPH_BUCKET': 'bucket',
+
+    #clearml
+    'clearml_url': 'url',
+    'clearml_access_key': 'access',
+    'clearml_secret_key': 'secret',
+    'clearml_username': 'testdario7',
+}
+
+task.connect(data_model_reg_cfg, name='model_data_cfg')
+
+print(data_model_reg_cfg)
+os.environ['CEPH_ENDPOINT'] = data_model_reg_cfg['CEPH_ENDPOINT']
+os.environ['CEPH_ACCESS_KEY'] = data_model_reg_cfg['CEPH_ACCESS_KEY']
+os.environ['CEPH_SECRET_KEY'] = data_model_reg_cfg['CEPH_SECRET_KEY']
+os.environ['CEPH_BUCKET'] = data_model_reg_cfg['CEPH_BUCKET']
+
+# print("CEPH_BUCKET:", os.environ["CEPH_BUCKET"])
+# print("CEPH_ENDPOINT:", os.environ["CEPH_ENDPOINT"])
+# print("CEPH_ACCESS_KEY:", os.environ["CEPH_ACCESS_KEY"])
+# print("CEPH_SECRET_KEY:", os.environ["CEPH_SECRET_KEY"])
+
 # --------- fetch model from model registry --------
 manager = MLOpsManager(
-    clearml_url="http://web.mlops.ai-lab.ir/api_old",
-    clearml_access_key="9E280YOU7E94HR84OMDPW71JBP2XPZ",
-    clearml_secret_key="uRzPKO078_7TGFJRzImk0Zj1AwefwHgpiV11IWr8joS5M6AzKhwTTKI_HEHhxylXPoA",
-    clearml_username="mlops-admin"
+    clearml_url=data_model_reg_cfg['clearml_url'],
+    clearml_access_key=data_model_reg_cfg['clearml_access_key'],
+    clearml_secret_key=data_model_reg_cfg['clearml_secret_key'],
+    clearml_username=data_model_reg_cfg['clearml_username']
 )
+
 
 
 # ---------- Variables -------------
@@ -32,16 +140,16 @@ model_save_name = "Autoformer_save1"
 model_id = "Autoformer"
 
 # Ensure valid model name/id
-if model_name not in ["Autoformer", "TimesNet"] and model_id not in ["Autoformer", "TimesNet"]:
-    raise ValueError("Invalid model name/id: choose from Autoformer or TimesNet")
+# if model_name not in ["Autoformer", "TimesNet"] and model_id not in ["Autoformer", "TimesNet"]:
+#     raise ValueError("Invalid model name/id: choose from Autoformer or TimesNet")
 
-# Dataset configuration
-if dataset == 'airline-passengers':
-    sources = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/airline-passengers.csv"
-elif dataset == "oil-spill":
-    sources = "https://raw.githubusercontent.com/jbrownlee/Datasets/refs/heads/master/oil-spill.csv"
-else:
-    raise ValueError("Invalid dataset: choose either 'airline-passengers' or 'oil-spill'")
+# # Dataset configuration
+# if dataset == 'airline-passengers':
+#     sources = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/airline-passengers.csv"
+# elif dataset == "oil-spill":
+#     sources = "https://raw.githubusercontent.com/jbrownlee/Datasets/refs/heads/master/oil-spill.csv"
+# else:
+#     raise ValueError("Invalid dataset: choose either 'airline-passengers' or 'oil-spill'")
 
 # --------------     to load model -----------------
 if load_model: 
@@ -52,13 +160,6 @@ if load_model:
         local_dest="."
     )
 
-# --------- ClearML task initialization --------
-task = Task.init(
-    project_name="API training",  # Name of the ClearML project
-    task_name=f"{dataset} - {model_name} - API Training",  # Name of the task
-    task_type=Task.TaskTypes.optimizer,  # Type of the task (could also be "training", "testing", etc.)
-    reuse_last_task_id=False  # Whether to reuse the last task ID (set to False for a new task each time)
-)
 #----------------- main config ----------------
 
 cfg = {
@@ -67,10 +168,13 @@ cfg = {
     "batch_size": batch_size,
     "seq_len": 12,
     "pred_len": 1,
-    "input_channels": 1,  # Added this
-    "output_size": 1,     # Added this
+    "input_channels": 1,  
+    "output_size": 1,     
+
     "dataset_config": {
-        "source": sources,
+        "name": 'dataset',
+        "source": 'url',
+        "target_column": 'target',
     },
 
     # Model save
@@ -91,7 +195,23 @@ cfg = {
     }
 }
 
+print(cfg)
+
 task.connect(cfg)
+url = get_dataset_download_urls(
+    # url="https://api.mlops.ai-lab.ir/data/download-dataset",
+    url="http://data-ingestion-api-service.aip-mlops-service.svc.cluster.local:8169/download-dataset",
+    # url="https://data-ingestion-api-service:8169/download-dataset",
+    dataset_name=cfg["dataset_config"]["name"],
+    user_name=data_model_reg_cfg['clearml_username'],
+    clearml_access_key=data_model_reg_cfg['clearml_access_key'],
+    clearml_secret_key=data_model_reg_cfg['clearml_secret_key'],
+    s3_access_key=data_model_reg_cfg['CEPH_ACCESS_KEY'],
+    s3_secret_key=data_model_reg_cfg['CEPH_SECRET_KEY'],
+    s3_endpoint_url=data_model_reg_cfg['CEPH_ENDPOINT']
+)
+
+cfg["dataset_config"]["source"] = url
 
 trainer = AutoTrainer(config=cfg)
 
