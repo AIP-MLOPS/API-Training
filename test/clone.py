20597b76177e85656349ea85cdb33f97052923f6
TheHypnotic <72342378+TheHypnotic@users.noreply.github.com>
2025-10-16 15:45:27 +0330

    cleaning

---
+++ test/clone.py
@@ -0,0 +1,21 @@
+from clearml import Task
+
+task = Task.init(
+    project_name="API training",
+    task_name=f"{dataset} - {model_name} - API Training",  
+    task_type=Task.TaskTypes.optimizer, 
+    reuse_last_task_id=False  
+)
+
+cfg = {
+    # Training Params
+    "task": "default",
+    "batch_size": "default",
+    "split_ratio": "default",
+    "lr": "default",
+    "epochs": "default",
+    "num_classes": "default",
+}
+task.connect(cfg)
+
+print(cfg)
