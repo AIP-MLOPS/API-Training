f26006068a5b8c958d25474ac9f1d01cb44e80d4
TheHypnotic <72342378+TheHypnotic@users.noreply.github.com>
2025-12-03 20:16:27 +0330

    debug

--- training-api/image_classification.py
+++ training-api/image_classification.py
@@ -177,6 +177,7 @@ s3_download(
         # user_name=data_model_reg_cfg['clearml_username'],
     )
 config["dataset_config"]["source"] = s3_download
+print(type(config["dataset_config"]["source"]))
 
 
 # absolute_path = Path(__file__).parent / "dataset" / config["dataset_config"]["source"] / "images"
